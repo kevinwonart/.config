f5eae87779351093d7e60bca63b31fc7285905df
Kevin Won <kevin.won91@gmail.com>
2023-12-24 14:22:46 -0600

    lazy vim

---
+++ nvim/init.lua
@@ -0,0 +1,49 @@
+vim.cmd("set shiftwidth=2 smarttab")
+vim.cmd("set expandtab")
+vim.cmd("set wrap linebreak nolist")
+vim.cmd("set number relativenumber")
+vim.cmd("set nu rnu")
+vim.cmd("set clipboard+=unnamedplus")
+vim.g.mapleader = " "
+vim.api.nvim_set_var('clipboard', {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("replace("`r", ""))',
+  },
+  cache_enabled = 0,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "ntk148v/habamax.nvim", name = "habamax", dependencies={ "rktjmp/lush.nvim" }, priority = 1000 },
+  { "catppuccin/nvim", priority = 1000 },
+  { 
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+}
+
+require("lazy").setup(plugins)
+local builtin = require("telescope.builtin")
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+require('lush')(require('lush_theme.habamax'))
+vim.cmd("colorscheme habamax")
---
+++ nvim/initbackup.vim
@@ -0,0 +1,19 @@
+set shiftwidth=2 smarttab
+set expandtab
+set wrap linebreak nolist
+set number relativenumber
+set nu rnu
+set clipboard+=unnamedplus
+
+let g:clipboard = {
+                \   'name': 'WslClipboard',
+                \   'copy': {
+                \      '+': 'clip.exe',
+                \      '*': 'clip.exe',
+                \    },
+                \   'paste': {
+                \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                \   },
+                \   'cache_enabled': 0,
+                \ }
