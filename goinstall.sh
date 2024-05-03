b94d6981d06ab2a29687e632be5ef400152b0789
kevin won <kevin.won91@gmail.com>
2024-05-03 00:05:53 -0500

    add npm to package list

---
+++ goinstall.sh
@@ -0,0 +1,6 @@
+## to remove old version
+## rm -rf /usr/local/go
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
